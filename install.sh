a4d4fda42202e062fc6fca628e1c7ed9d9c566fe
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2016-09-29 10:43:43 +0200

    Update install

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-sudo apt-get install -y git vim
-rm -rf ~/.vim/bundle/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-install -m 644 ${0%/*}/vimrc ~/.vimrc
-mkdir -p ~/.vim/backup
-vim -E -s -c "source ~/.vimrc" -c PluginInstall -c qa
